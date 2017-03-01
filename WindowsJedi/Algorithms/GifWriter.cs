b4dd139cd28b0ff939a6e1cacbe9ad1334052706
Iain Ballard <iain.ballard@gmail.com>
2017-03-01 14:06:58 +0000

    Working gif capture. Need optimisation

--- WindowsJedi/Algorithms/GifWriter.cs
+++ WindowsJedi/Algorithms/GifWriter.cs
@@ -6,6 +6,7 @@ namespace WindowsJedi.Algorithms
 {
     using System;
     using System.Text;
+    using System.Windows.Forms;
 
     public class GifWriter : IDisposable
     {
@@ -13,6 +14,7 @@ namespace WindowsJedi.Algorithms
         readonly BinaryWriter _fileWriter;
         bool _firstFrame;
         readonly Size _size;
+        readonly bool _drawCursor;
         readonly Bitmap _screenBuffer; // IDEA: copy using a small (32x32?) tile at a higher rate?
         readonly MemoryStream _buffer;
 
@@ -42,7 +44,7 @@ namespace WindowsJedi.Algorithms
                  249,  //Graphic control extension
                  4,    //Size of block
                  9,    //Flags: reserved, disposal method, user input, transparent color
-                 1,    //Delay time low byte
+                 5,    //Delay time low byte (hundredths of a second -- this relates to 20 fps)
                  0,    //Delay time high byte
                  255,  //Transparent color index
                  0     //Block terminator
@@ -52,10 +54,11 @@ namespace WindowsJedi.Algorithms
         /// Start an animated GIF with the given size and file name.
         /// Will immediately open the file, but headers are written on first frame
         /// </summary>
-        public GifWriter(string fileName, Size size)
+        public GifWriter(string fileName, Size size, bool drawCursor)
         {
             _buffer = new MemoryStream();
             _size = size;
+            _drawCursor = drawCursor;
             _firstFrame = true;
             _fileStream = File.Open(fileName, FileMode.Create, FileAccess.ReadWrite, FileShare.None);
             _fileWriter = new BinaryWriter(_fileStream, Encoding.ASCII);
@@ -96,6 +99,12 @@ namespace WindowsJedi.Algorithms
             using (var g = Graphics.FromImage(_screenBuffer))
             {
                 g.CopyFromScreen(topLeft, Point.Empty, _size);
+
+                if (_drawCursor && Cursor.Current != null) { // this doesn't draw the correct cursor, but at least it shows *something*
+                    var pt = new Point(Cursor.Position.X - topLeft.X - Cursor.Current.HotSpot.X, Cursor.Position.Y - topLeft.Y - Cursor.Current.HotSpot.Y);
+                    var rect = new Rectangle(pt, Cursor.Current.Size);
+                    Cursor.Current.Draw(g, rect);
+                }
             }
             WriteImageFrame(_screenBuffer);
         }
@@ -104,7 +113,7 @@ namespace WindowsJedi.Algorithms
         {
             _buffer.SetLength(0);
 
-            frame.Save(_buffer, ImageFormat.Gif); // This makes a mes of the dithering. To be improved... ( http://codebetter.com/brendantompkins/2007/06/14/gif-image-color-quantizer-now-with-safe-goodness/ )
+            frame.Save(_buffer, ImageFormat.Gif); // This makes a mess of the dithering. To be improved... ( http://codebetter.com/brendantompkins/2007/06/14/gif-image-color-quantizer-now-with-safe-goodness/ )
             var gifFrame = _buffer.ToArray();
 
             if (_firstFrame)
--- WindowsJedi/Features/ScreenCaptureForm.cs
+++ WindowsJedi/Features/ScreenCaptureForm.cs
@@ -6,20 +6,82 @@
     using System.Drawing.Imaging;
     using System.Drawing.Text;
     using System.Windows.Forms;
+    using WindowsJedi.Algorithms;
     using WindowsJedi.Components;
     using WindowsJedi.WinApis;
 
     public class ScreenCaptureForm : OverlayForm
     {
-        //readonly Rectangle _rect;
+        GifWriter _outputFile;
+        bool _disposed;
+        readonly Timer _recordingTimer;
+        const int FPS_20 = 50;//ms
 
         public ScreenCaptureForm()
         {
-            // TODO: show a file save dialog to pick the target
+            _disposed = false;
+            _recordingTimer = new Timer { Enabled = false, Interval = FPS_20 };
+            _recordingTimer.Tick += _recordingTimer_Tick;
+
             FormBorderStyle = FormBorderStyle.None;
             MaximizeBox = false;
             MinimizeBox = false;
             SetBounds(20, 200, 320, 240);
+            UpdateOverlay();
+        }
+
+        void _recordingTimer_Tick(object sender, EventArgs e)
+        {
+            if (_outputFile != null)
+            {
+                _outputFile.WriteScreenFrame(new Point(Left + 11, Top + 11));
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            if (_disposed) return;
+            _disposed = true;
+            if (_outputFile != null)
+            {
+                _outputFile.Dispose();
+            }
+            base.OnClosed(e);
+        }
+
+        void ChooseFile()
+        {
+            var dlog = new SaveFileDialog();
+            dlog.OverwritePrompt = true;
+            dlog.DefaultExt = "gif";
+            dlog.AddExtension = true;
+            var result = dlog.ShowDialog();
+            switch (result) {
+                case DialogResult.OK:
+                case DialogResult.Yes:
+                    {
+                        if (_outputFile != null)
+                        {
+                            _outputFile.Dispose();
+                        }
+                        _outputFile = new GifWriter(dlog.FileName, new Size(Width - 21, Height - 31), true);
+                        UpdateOverlay();
+                        break;
+                    }
+                default:
+                    return;
+            }
+        }
+
+        void AddFrame()
+        {
+            if (_recordingTimer.Enabled) return;
+            _outputFile.WriteScreenFrame(new Point(Left + 11, Top + 11));
+        }
+
+        void ToggleRecording()
+        {
+            _recordingTimer.Enabled = !_recordingTimer.Enabled;
             UpdateOverlay();
         }
 
@@ -61,20 +123,62 @@
                 g.DrawRectangle(Pens.Black, 10,10,Width-20,Height-30);
 
                 // resize chevrons
-                g.DrawLine(Pens.Black, Width, Height - 18, Width - 18, Height);
-                g.DrawLine(Pens.Black, Width, Height - 14, Width - 14, Height);
-                g.DrawLine(Pens.Black, Width, Height - 10, Width - 10, Height);
-                g.DrawLine(Pens.Black, Width, Height - 6, Width - 6, Height);
+                if (_outputFile == null)
+                {
+                    g.DrawLine(Pens.Black, Width, Height - 18, Width - 18, Height);
+                    g.DrawLine(Pens.Black, Width, Height - 14, Width - 14, Height);
+                    g.DrawLine(Pens.Black, Width, Height - 10, Width - 10, Height);
+                    g.DrawLine(Pens.Black, Width, Height - 6, Width - 6, Height);
+                }
 
                 // close cross
-                g.DrawLine(Pens.Black, 0, 0, 10, 10);
-                g.DrawLine(Pens.Black, 0, 10, 10, 0);
+                g.DrawLine(Pens.Black, 2, 2, 9, 9);
+                g.DrawLine(Pens.Black, 2, 9, 9, 2);
 
-                // TODO: buttons to toggle recording and to snap one frame
+                // Set file icon
+                DrawPageIcon(g, 10, Height - 17);
+
+                // record icon
+                DrawRecordIcon(g, 25, Height - 17);
+
+                // snap frame icon
+                DrawSnapFrameIcon(g, 40, Height - 17);
             }
             return b;
         }
 
+        static void DrawSnapFrameIcon(Graphics g, int x, int y)
+        {
+            var p = Pens.Black;
+            g.DrawRectangle(p, x, y, 7, 7);
+            
+            g.DrawLine(p, x+11, y, x+11, y+11);
+            g.DrawLine(p, x, y + 11, x + 11, y + 11);
+            g.DrawLine(p, x + 9, y, x + 9, y + 9);
+            g.DrawLine(p, x, y + 9, x + 9, y + 9);
+
+            g.FillRectangle(Brushes.Black, x+2.5f, y, 2, 7);
+            g.FillRectangle(Brushes.Black, x, y+2.5f, 7, 2);
+        }
+
+        void DrawRecordIcon(Graphics g, int x, int y)
+        {
+            g.DrawRectangle(Pens.Black, x, y, 11, 11);
+            if (_recordingTimer.Enabled) g.FillRectangle(Brushes.Black, x+2, y+2, 7, 7);
+            else g.FillEllipse(Brushes.Red, x+2, y+2, 7, 7);
+        }
+
+        static void DrawPageIcon(Graphics g, int x, int y) {
+            var p = Pens.Black;
+            g.DrawLine(p, x    , y     , x + 6, y);
+            g.DrawLine(p, x + 9, y + 3 , x + 9, y + 11);
+            g.DrawLine(p, x + 9, y + 11, x    , y + 11);
+            g.DrawLine(p, x    , y + 11, x    , y);
+
+            g.DrawLine(p, x + 6, y  , x + 6, y + 3);
+            g.DrawLine(p, x + 6, y  , x + 9, y + 3);
+            g.DrawLine(p, x + 6, y+3, x + 9, y + 3);
+        }
 
         protected override void OnPaint(PaintEventArgs e)
         {
@@ -85,40 +189,46 @@
             UpdateOverlay();
         }
 
+        static bool InRect(Point p, int l, int t, int r, int b)
+        {
+            return p.X >= l && p.X <= r && p.Y >= t && p.Y <= b;
+        }
 
         protected override void WndProc(ref Message m)
         {
+            if (_disposed) return;
             switch (m.Msg)
             {
                 case Win32.WM_NCHITTEST: // screen-space coords
                     {
                         var c = PointToClient(GetPoint(m.LParam));
-                        if (c.X > Width - 10 && c.Y > Height - 10) m.Result = Win32.HTBOTTOMRIGHT;
-                        else if (c.X < 10 && c.Y < 10) m.Result = Win32.HTCLIENT;
+                        var sizeEnabled = _outputFile == null; // disable resize once we start a file
+
+                        if (sizeEnabled && c.X > Width - 10 && c.Y > Height - 10) m.Result = Win32.HTBOTTOMRIGHT;
+                        else if (
+                            InRect(c, 0, 0, 10, 10) ||
+                            InRect(c, 10, Height - 17, 22, Height) ||
+                            InRect(c, 25, Height - 17, 37, Height) ||
+                            InRect(c, 40, Height - 17, 52, Height)
+                            ) m.Result = Win32.HTCLIENT; // If you don't return HTCLIENT, you won't get the button events
                         else m.Result = Win32.HTCAPTION;
                         return;
                     }
                 case Win32.WM_LBUTTONUP: // client-space coords
                     {
                         var c = GetPoint(m.LParam);
-                        if (c.X < 10 && c.Y < 10) Close();
+                        if (InRect(c,0,0,10,10)) { 
+                            Close();
+                            return;
+                        }
+                        
+                        if (InRect(c, 10, Height - 17, 22, Height)) { ChooseFile(); }
+                        else if (InRect(c, 25, Height - 17, 37, Height)) { ToggleRecording(); }
+                        else if (InRect(c, 40, Height - 17, 52, Height)) { AddFrame(); }
+
+                        base.WndProc(ref m);
                         return;
                     }
-                /*case Win32.WM_MOUSEMOVE:
-                    {
-                        var p = GetPoint(m.LParam);
-                        var c = PointToClient(GetPoint(m.LParam));
-
-                        if (c.X > Width - 10 && c.Y > Height - 10)
-                        {
-
-                        }
-                        else
-                        {
-                            SetDesktopLocation(p.X, p.Y);
-                        }
-                        return;
-                    }*/
                 default:
                     base.WndProc(ref m);
                     return;
